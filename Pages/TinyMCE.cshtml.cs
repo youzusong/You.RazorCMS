6bdc075a8a1ffc9d469cc31485aaebf7a34289d4
youzusong <youzusong@outlook.com>
2024-10-28 17:48:38 +0800

    20241028

---
+++ Pages/TinyMCE.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace You.RazorCMS.Pages
+{
+    public class TinyMCEModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
